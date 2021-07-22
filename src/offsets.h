42fa05e72535ee89ca9ceb6d98669f1c6aacff77
Rafael Ortiz <rafael.ortiz@redcanary.com>
2021-07-22 15:51:14 -0400

    begone mystery slash

--- src/offsets.h
+++ src/offsets.h
@@ -10,5 +10,5 @@ const u64 CRC_TASK_STRUCT_REAL_PARENT = 0x940b92aaad4c5437;
 /* CRC64 of task_struct->pid */
 const u64 CRC_TASK_STRUCT_PID = 0xc713ffcffcd1cc3c;
 
-/* CRC64 of task_struct->loginuid */                                                                        \
+/* CRC64 of task_struct->loginuid */
 const u64 CRC_TASK_STRUCT_LOGINUID = 0x9951a3e4f7757060;
