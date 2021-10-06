893154b086767030ee56cce6dce41899f26743e3
rc-dbogle <dave.bogle@redcanary.com>
2021-10-06 13:56:09 +0000

    Combined tcpv4 and tcpv6

--- src/programs.c
+++ src/programs.c
@@ -152,16 +152,7 @@ struct bpf_map_def SEC("maps/telemetry_events") telemetry_events = {
     .namespace = "",
 };
 
-struct bpf_map_def SEC("maps/tcpv4_connect") tcpv4_connect = {
-    .type = BPF_MAP_TYPE_HASH,
-    .key_size = sizeof(u32),
-    .value_size = sizeof(size_t),
-    .max_entries = 1024,
-    .pinning = 0,
-    .namespace = "",
-};
-
-struct bpf_map_def SEC("maps/tcpv6_connect") tcpv6_connect = {
+struct bpf_map_def SEC("maps/tcp_connect") tcp_connect = {
     .type = BPF_MAP_TYPE_HASH,
     .key_size = sizeof(u32),
     .value_size = sizeof(size_t),
@@ -1301,24 +1292,13 @@ static __always_inline int save_sock_ptr(struct pt_regs *ctx, void *map)
     return 0;
 }
 
-SEC("kprobe/tcp_v4_connect")
-int kprobe__tcp_v4_connect(struct pt_regs *ctx)
+SEC("kprobe/tcp_connect")
+int kprobe__tcp_connect(struct pt_regs *ctx)
 {
     _sock sk = (_sock)PT_REGS_PARM1(ctx);
     u32 index = (u32)bpf_get_current_pid_tgid();
 
-    bpf_map_update_elem(&tcpv4_connect, &index, &sk, BPF_ANY);
-
-    return 0;
-}
-
-SEC("kprobe/tcp_v6_connect")
-int kprobe__tcp_v6_connect(struct pt_regs *ctx)
-{
-    _sock sk = (_sock)PT_REGS_PARM1(ctx);
-    u32 index = (u32)bpf_get_current_pid_tgid();
-
-    bpf_map_update_elem(&tcpv6_connect, &index, &sk, BPF_ANY);
+    bpf_map_update_elem(&tcp_connect, &index, &sk, BPF_ANY);
 
     return 0;
 }
@@ -1403,7 +1383,6 @@ int kretprobe__ret_udp_v6_send_skb(struct pt_regs *ctx)
     // Save the pid in the event structure
     ev.u.network_info.process.pid = index;
 
-    // Lookup the corresponding *sk that we saved when tcp_v4_connect was called
     skpp = bpf_map_lookup_elem(&udpv6_outgoing_map, &index);
     if (skpp == NULL)
     {
@@ -1503,7 +1482,6 @@ int kretprobe__ret_ip6_local_out(struct pt_regs *ctx)
     // Save the pid in the event structure
     ev.u.network_info.process.pid = index;
 
-    // Lookup the corresponding *sk that we saved when tcp_v4_connect was called
     skpp = bpf_map_lookup_elem(&udpv6_outgoing_map, &index);
     if (skpp == NULL)
     {
@@ -1631,80 +1609,6 @@ int kretprobe__ret_inet_csk_accept(struct pt_regs *ctx)
     return 0;
 }
 
-SEC("kretprobe/ret_tcp_v4_connect")
-int kretprobe__ret_tcp_v4_connect(struct pt_regs *ctx)
-{
-    // Get the return value from tcp_v4_connect
-    int ret = PT_REGS_RC(ctx);
-
-    /* if "loaded" doesn't exist in the map, we get NULL back and won't read from offsets              
-     * when offsets are loaded into the offsets map, "loaded" should be given any value                
-     */
-    u64 loaded = CRC_LOADED; // CRC64 of "loaded"
-    loaded = (u64)bpf_map_lookup_elem(&offsets, &loaded);
-
-    // Just to be safe 0 out the structs
-    telemetry_event_t ev;
-    memset(&ev, 0, sizeof(ev));
-
-    // Initialize some of the telemetry event
-    ev.id = bpf_get_prandom_u32();
-    ev.done = 0;
-    ev.telemetry_type = TE_NETWORK;
-    ev.u.network_info.mono_ns = bpf_ktime_get_ns();
-
-    // Get current pid
-    u32 index = (u32)bpf_get_current_pid_tgid();
-    _sock *skpp;
-
-    // Lookup the corresponding *sk that we saved when tcp_v4_connect was called
-    skpp = bpf_map_lookup_elem(&tcpv4_connect, &index);
-    if (skpp == 0)
-    {
-        return 0;
-    }
-    bpf_map_delete_elem(&tcpv4_connect, &index);
-
-    // Deref
-    _sock skp = *skpp;
-    unsigned char *skp_base = (unsigned char *)skp;
-    if (skp_base == NULL)
-    {
-        return 0;
-    }
-
-    // failed to send SYNC packet, may not have populated
-    // socket __sk_common.{skc_rcv_saddr, ...}
-    if (ret != 0)
-    {
-        return 0;
-    }
-
-    // TODO: Need to integrate this with lkccb to get correct offsets
-    // TODO: Since we are tracking connect isn't this always outbound?
-    ev.u.network_info.direction = outbound;
-    ev.u.network_info.protocol_type = IPPROTO_TCP;
-    ev.u.network_info.ip_type = AF_INET;
-
-    if (loaded && skp_base)
-    {
-        read_value(skp_base, CRC_SOCK_COMMON_DADDR, &ev.u.network_info.protos.ipv4.dest_addr, sizeof(ev.u.network_info.protos.ipv4.dest_addr));
-        read_value(skp_base, CRC_SOCK_COMMON_SADDR, &ev.u.network_info.protos.ipv4.src_addr, sizeof(ev.u.network_info.protos.ipv4.src_addr));
-        read_value(skp_base, CRC_SOCK_COMMON_DPORT, &ev.u.network_info.dest_port, sizeof(ev.u.network_info.dest_port));
-        read_value(skp_base, CRC_SOCK_COMMON_SPORT, &ev.u.network_info.src_port, sizeof(ev.u.network_info.src_port));
-        ev.u.network_info.dest_port = SWAP_U16(ev.u.network_info.dest_port); // Get the endianness right before returning it
-    }
-
-    // Get Process data and set pid and comm string
-    ev.u.network_info.process.pid = index;
-    bpf_get_current_comm(ev.u.network_info.process.comm, sizeof(ev.u.network_info.process.comm));
-
-    // Output data to generator
-    bpf_perf_event_output(ctx, &telemetry_events, bpf_get_smp_processor_id(), &ev, sizeof(ev));
-
-    return 0;
-}
-
 // This handles IPv4 udp packets
 SEC("kretprobe/ret_udp_rcv")
 int kretprobe__ret_udp_rcv(struct pt_regs *ctx)
@@ -1731,7 +1635,6 @@ int kretprobe__ret_udp_rcv(struct pt_regs *ctx)
     // Save the pid in the event structure
     ev.u.network_info.process.pid = index;
 
-    // Lookup the corresponding *sk that we saved when tcp_v4_connect was called
     skpp = bpf_map_lookup_elem(&udpv4_rcv_map, &index);
     if (skpp == NULL)
     {
@@ -2097,8 +2000,8 @@ int kretprobe__ret_ip_local_out(struct pt_regs *ctx)
     return 0;
 }
 
-SEC("kretprobe/ret_tcp_v6_connect")
-int kretprobe__ret_tcp_v6_connect(struct pt_regs *ctx)
+SEC("kretprobe/ret_tcp_connect")
+int kretprobe__ret_tcp_connect(struct pt_regs *ctx)
 {
     int ret = PT_REGS_RC(ctx);
     if (ret != 0)
@@ -2108,6 +2011,10 @@ int kretprobe__ret_tcp_v6_connect(struct pt_regs *ctx)
 
     u64 loaded = CRC_LOADED;
     loaded = (u64)bpf_map_lookup_elem(&offsets, &loaded); /* squeezing out as much stack as possible */
+    if (!loaded)
+    {
+        return 0;
+    }
 
     // Just to be safe 0 out the structs
     telemetry_event_t ev;
@@ -2123,13 +2030,14 @@ int kretprobe__ret_tcp_v6_connect(struct pt_regs *ctx)
     u32 index = (u32)bpf_get_current_pid_tgid();
     _sock *skpp;
 
-    skpp = bpf_map_lookup_elem(&tcpv6_connect, &index);
+    // if ipv4 do one thing else do the other
+
+    skpp = bpf_map_lookup_elem(&tcp_connect, &index);
     if (skpp == 0)
     {
         return 0;
     }
-
-    bpf_map_delete_elem(&tcpv6_connect, &index);
+    bpf_map_delete_elem(&tcp_connect, &index);
 
     // Deref
     _sock skp = *skpp;
@@ -2139,18 +2047,27 @@ int kretprobe__ret_tcp_v6_connect(struct pt_regs *ctx)
         return 0;
     }
 
+    // Connect calls are always outbound and this function is for tcp
     ev.u.network_info.direction = outbound;
     ev.u.network_info.protocol_type = IPPROTO_TCP;
-    ev.u.network_info.ip_type = AF_INET6;
 
-    if (loaded)
+    // Get the ip type and then
+    read_value(skp_base, CRC_SOCK_COMMON_FAMILY, &ev.u.network_info.ip_type, sizeof(ev.u.network_info.ip_type));
+    if (ev.u.network_info.ip_type == AF_INET)
+    {
+        read_value(skp_base, CRC_SOCK_COMMON_DADDR, &ev.u.network_info.protos.ipv4.dest_addr, sizeof(ev.u.network_info.protos.ipv4.dest_addr));
+        read_value(skp_base, CRC_SOCK_COMMON_SADDR, &ev.u.network_info.protos.ipv4.src_addr, sizeof(ev.u.network_info.protos.ipv4.src_addr));
+    }
+    else
     {
         read_value(skp_base, CRC_SOCK_COMMON_DADDR6, &ev.u.network_info.protos.ipv6.dest_addr, sizeof(ev.u.network_info.protos.ipv6.dest_addr));
         read_value(skp_base, CRC_SOCK_COMMON_SADDR6, &ev.u.network_info.protos.ipv6.src_addr, sizeof(ev.u.network_info.protos.ipv6.src_addr));
-        read_value(skp_base, CRC_SOCK_COMMON_DPORT, &ev.u.network_info.dest_port, sizeof(ev.u.network_info.dest_port));
-        read_value(skp_base, CRC_SOCK_COMMON_SPORT, &ev.u.network_info.src_port, sizeof(ev.u.network_info.src_port));
-        ev.u.network_info.dest_port = SWAP_U16(ev.u.network_info.dest_port);
     }
+
+    // Get port info
+    read_value(skp_base, CRC_SOCK_COMMON_DPORT, &ev.u.network_info.dest_port, sizeof(ev.u.network_info.dest_port));
+    read_value(skp_base, CRC_SOCK_COMMON_SPORT, &ev.u.network_info.src_port, sizeof(ev.u.network_info.src_port));
+    ev.u.network_info.dest_port = SWAP_U16(ev.u.network_info.dest_port);
 
     // Get Process data and set pid and comm string
     ev.u.network_info.process.pid = index;
