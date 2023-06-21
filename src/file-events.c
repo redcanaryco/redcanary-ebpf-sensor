ac89e45c931226bde4aad5762760a7a1cab246d2
Andrés Medina <andres.medina@redcanary.com>
2023-06-21 11:50:51 -0700

    add chmod-like events

--- src/file-events.c
+++ src/file-events.c
@@ -11,6 +11,7 @@
 #include "common/bpf_helpers.h"
 #include "file/create.h"
 #include "file/delete.h"
+#include "file/modify.h"
 
 /* START CREATE-LIKE PROBES */
 
@@ -119,14 +120,8 @@ struct syscalls_exit_args {
     long ret;
 };
 
-struct syscalls_enter_unlink_args {
-    __u64 unused;
-    long __syscall_nr;
-    const char *pathname;
-};
-
 SEC("tracepoint/sys_enter_unlink")
-int tracepoint__syscalls_sys_enter__unlink(struct syscalls_enter_unlink_args *ctx) {
+int tracepoint__syscalls_sys_enter__unlink(void *ctx) {
     enter_delete(ctx);
     return 0;
 }
@@ -134,20 +129,12 @@ int tracepoint__syscalls_sys_enter__unlink(struct syscalls_enter_unlink_args *ct
 SEC("tracepoint/sys_exit_unlink")
 int tracepoint__syscalls_sys_exit__unlink(struct syscalls_exit_args *ctx) {
     if (ctx->ret < 0) return 0;
-    bpf_tail_call(ctx, &tp_programs, EXIT_DELETE);
+    prepare_delete(ctx);
     return 0;
 }
 
-struct syscalls_enter_unlinkat_args {
-    __u64 unused;
-    long __syscall_nr;
-    long dfd;
-    const char *pathname;
-    long flag;
-};
-
 SEC("tracepoint/sys_enter_unlinkat")
-int tracepoint__syscalls_sys_enter__unlinkat(struct syscalls_enter_unlinkat_args *ctx) {
+int tracepoint__syscalls_sys_enter__unlinkat(void *ctx) {
     enter_delete(ctx);
     return 0;
 }
@@ -155,7 +142,7 @@ int tracepoint__syscalls_sys_enter__unlinkat(struct syscalls_enter_unlinkat_args
 SEC("tracepoint/sys_exit_unlinkat")
 int tracepoint__syscalls_sys_exit__unlinkat(struct syscalls_exit_args *ctx) {
     if (ctx->ret < 0) return 0;
-    bpf_tail_call(ctx, &tp_programs, EXIT_DELETE);
+    prepare_delete(ctx);
     return 0;
 }
 
@@ -165,14 +152,8 @@ int BPF_KPROBE(security_path_unlink, const struct path *dir, struct dentry *dent
     return 0;
 }
 
-struct syscalls_enter_rmdir_args {
-    __u64 unused;
-    long __syscall_nr;
-    const char *pathname;
-};
-
 SEC("tracepoint/sys_enter_rmdir")
-int tracepoint__syscalls_sys_enter__rmdir(struct syscalls_enter_rmdir_args *ctx) {
+int tracepoint__syscalls_sys_enter__rmdir(void *ctx) {
     enter_delete(ctx);
     return 0;
 }
@@ -186,17 +167,102 @@ int BPF_KPROBE(security_path_rmdir, const struct path *dir, struct dentry *dentr
 SEC("tracepoint/sys_exit_rmdir")
 int tracepoint__syscalls_sys_exit__rmdir(struct syscalls_exit_args *ctx) {
     if (ctx->ret < 0) return 0;
-    bpf_tail_call(ctx, &tp_programs, EXIT_DELETE);
-    return 0;
-}
-
-SEC("tracepoint/exit_delete")
-int tracepoint__exit_delete(void *ctx) {
-    exit_delete(ctx);
+    prepare_delete(ctx);
     return 0;
 }
 
 /* END DELETE-LIKE PROBES */
+
+/* START CHDMOD-LIKE */
+
+SEC("tracepoint/sys_enter_chmod")
+int tracepoint__syscalls_sys_enter__chmod(void *ctx) {
+    enter_open(ctx);
+    return 0;
+}
+
+SEC("tracepoint/sys_exit_chmod")
+int tracepoint__syscalls_sys_exit__chmod(struct syscalls_exit_args *ctx) {
+    if (ctx->ret < 0) return 0;
+    prepare_modify(ctx);
+    return 0;
+}
+
+SEC("tracepoint/sys_enter_fchmod")
+int tracepoint__syscalls_sys_enter__fchmod(void *ctx) {
+    enter_open(ctx);
+    return 0;
+}
+
+SEC("tracepoint/sys_exit_fchmod")
+int tracepoint__syscalls_sys_exit__fchmod(struct syscalls_exit_args *ctx) {
+    if (ctx->ret < 0) return 0;
+    prepare_modify(ctx);
+    return 0;
+}
+
+SEC("tracepoint/sys_enter_fchmodat")
+int tracepoint__syscalls_sys_enter__fchmodat(void *ctx) {
+    enter_open(ctx);
+    return 0;
+}
+
+SEC("tracepoint/sys_exit_fchmodat")
+int tracepoint__syscalls_sys_exit__fchmodat(struct syscalls_exit_args *ctx) {
+    if (ctx->ret < 0) return 0;
+    prepare_modify(ctx);
+    return 0;
+}
+
+SEC("kprobe/security_path_chmod")
+int BPF_KPROBE(security_path_chmod, const struct path *path, umode_t mode) {
+    store_modified_dentry(ctx, (void *)path);
+    return 0;
+}
+
+/* END CHMOD-LIKE PROBES */
+
+
+static __always_inline void filemod_paths(void *ctx)
+{
+    u32 key = 0;
+    buf_t *buffer = (buf_t *)bpf_map_lookup_elem(&buffers, &key);
+    if (buffer == NULL) return;
+
+    cached_path_t *cached_path = (cached_path_t *)bpf_map_lookup_elem(&percpu_path, &key);
+    if (cached_path == NULL) return;
+
+    file_message_t *fm = (file_message_t *)buffer;
+    cursor_t cursor = { .buffer = buffer, .offset = &fm->u.action.buffer_len };
+
+    int ret = write_path(ctx, cached_path, &cursor, (tail_call_t){
+            .slot = FILE_PATHS,
+            .table = &tp_programs,
+        });
+    if (ret < 0) goto EmitWarning;
+    fm->u.action.tag = (cached_path->filter_state >= 0) ? cached_path->filter_tag : -1;
+    if (cached_path->filter_state <= 0) goto NoEvent; // Didn't match watched path filter
+
+    push_flexible_file_message(ctx, fm, *cursor.offset);
+
+    // lookup tail calls completed; ensure we re-init cached_path next call
+    cached_path->next_dentry = NULL;
+    return;
+
+ EmitWarning:
+    push_file_warning(ctx, fm, fm->type);
+
+ NoEvent:
+    // lookup tail calls completed; ensure we re-init cached_path next call
+    cached_path->next_dentry = NULL;
+    return;
+ }
+
+SEC("tracepoint/filemod_paths")
+int tracepoint__filemod_paths(void *ctx) {
+    filemod_paths(ctx);
+    return 0;
+}
 
 char _license[] SEC("license") = "GPL";
 uint32_t _version SEC("version") = 0xFFFFFFFE;
